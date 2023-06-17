52e9c0f317e7527690fa1ffbebc3a57d86b37250
Markus Graf <graf.markus10@gmx.at>
2023-06-17 18:37:23 +0200

    minor change

--- WebsocketInternalApi/src/main/kotlin/org/gateway/websocketInternalApi/messages/helper/BaseMessage.kt
+++ WebsocketInternalApi/src/main/kotlin/org/gateway/websocketInternalApi/messages/helper/BaseMessage.kt
@@ -1,6 +1,11 @@
 package org.gateway.websocketInternalApi.messages.helper
 
 import kotlinx.serialization.Serializable
+import org.gateway.utils.SystemConfiguration
 
 @Serializable
-abstract class BaseMessage(val text: String, val type: MessageType)
+abstract class BaseMessage(
+    val text: String,
+    val type: MessageType,
+    val gatewaySerialNumber: String = SystemConfiguration.serialNumber
+)
